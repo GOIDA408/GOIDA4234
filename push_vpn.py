3b80bb737ab79a5cbe632a932037a70f7c257d7d
GOIDA408 <legendary02peep@gmail.com>
2026-05-30 22:53:05 +0500

    Update push_vpn.py

---
+++ push_vpn.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Push parser.py в GOIDA408/PARSER и GOIDA408/GOIDA4234. Токен в .env"""
+from deploy_github import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
