17646613e15c043c12f0f2354eb561bae5c1f65f
GOIDA408 <legendary02peep@gmail.com>
2026-05-30 23:12:10 +0500

    Remove push_vpn.py from subscription repo

--- push_vpn.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""Push parser.py в GOIDA408/PARSER и GOIDA408/GOIDA4234. Токен в .env"""
-from deploy_github import main
-
-if __name__ == "__main__":
-    raise SystemExit(main())
